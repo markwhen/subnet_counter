0cba58d3e94758b56b46ea7a14567678f42af81c
Maojia Sheng <smj-10@mails.tsinghua.edu.cn>
2015-04-23 22:42:25 +0800

    Update subnet_count.py

--- subnet_count.py
+++ subnet_count.py
@@ -5,6 +5,7 @@ from optparse import OptionParser
 import os
 import threading
 import commands
+import time
 
 alive_num = 0
 last_alive_index = 0
@@ -36,6 +37,7 @@ def main():
     parser = OptionParser()
     parser.add_option("-m", "--multithread", dest="multithread", help="number of threads for concurrency, default is 10")
     parser.add_option("-t", "--target", dest="target", help="Target ip in network to scan -t 192.168.192.0.10/24")
+    parser.add_option("-s", "--sleeping", dest="sleeping", help="Seconds to sleep after one cycle, default is 1")
     parser.add_option("-f", "--filename", dest="filename", help="the file to record recent counting result")
     parser.add_option("-j", "--maxjump", dest="maxjump", help="if more than MAXJUMP continuous IPv4 addresses not respond, stop")
     parser.add_option("-w", "--timeout", dest="timeout", help="Ping timeout, default: 2")
@@ -51,6 +53,7 @@ def main():
     timeout = (options.timeout if options.timeout else '1')
     count = (options.count if options.count else '1')
     multithread = (int(options.multithread) if options.multithread else 10)
+    sleeping = (int(options.sleeping) if options.sleeping else 1)
     target = options.target
     filename = (options.filename if options.filename else './subnet_count')
     maxjump = (int(options.maxjump) if options.maxjump else 256*256*256*256)
@@ -93,6 +96,7 @@ def main():
         thread_pool[:] = []
         commands.getstatusoutput("echo "+str(alive_num)+" > "+filename)
         print "find "+str(alive_num)+" alive ipv4 addresses"
+        time.sleep(sleeping)
 
 if __name__ == '__main__':
     main()
